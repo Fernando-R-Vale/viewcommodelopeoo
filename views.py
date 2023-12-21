cc7df99a1e07c2d5db833055fc606a12658c2099
Fernando Rego do Vale <121295486+Fernando-R-Vale@users.noreply.github.com>
2023-12-21 19:00:41 -0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,69 @@
+from adm import adm, Nadm
+from models.departamento import departamento, Ndepartamento
+from models.universisdade import Universidade, NUniversidades
+from models.curso import curso, Ncurso
+class View:
+  def Adm_inserir(email, senha):
+    Adm = adm(0, email, senha)
+    Nadm.inserir(Adm)
+
+  def Adm_listar():
+    return Nadm.listar()
+  
+  def Adm_listar_id(id):
+    return Nadm.listar_id(id)
+
+  def Adm_atualizar(id, email, senha):
+    Adm = adm(id, email, senha)
+    Nadm.atualizar(Adm)
+    
+  def Adm_excluir(id):
+    Adm = adm(id, "", "")
+    Nadm.excluir(Adm)  
+
+  def Adm_login(email,senha):
+    a = adm(0, email, senha)
+    for i in Nadm.listar():
+      if i.get_email() == a.get_email() and i.get_senha() == a.get_senha():
+        return True
+        break
+      else: pass
+
+  def departamento_listar():
+    return Ndepartamento.listar()
+
+  def departamento_listar_id(id):
+    return Ndepartamento.listar_id(id)
+
+  def departamento_inserir(nome, id_cursos, telefone):
+    Ndepartamento.inserir(departamento(0, nome, id_cursos, telefone))
+
+  def departamento_atualizar(id, nome, id_cursos, telefone):
+    Ndepartamento.atualizar(departamento(id, nome, id_cursos, telefone))
+
+  def departamento_excluir(id):
+    Ndepartamento.excluir(departamento(id, "", 0, ""))
+
+  def universidade_listar():
+    return NUniversidades.listar()
+
+  def universidade_inserir(nome, local, id_departamento, telefone):
+    NUniversidades.inserir(Universidade(0, nome, local, id_departamento, telefone))
+
+  def universidade_atualizar(id, nome, local, id_departamento, telefone):
+    NUniversidades.atualizar(Universidade(id, nome, local, id_departamento, telefone))
+
+  def universidade_excluir(id):
+    NUniversidades.excluir(Universidade(id, "", "", 0, ""))
+
+  def curso_listar():
+    return Ncurso.listar()
+
+  def curso_inserir(nome, professores, vagas, nivel):
+    Ncurso.inserir(curso(0, nome, professores, vagas, nivel))
+
+  def curso_atualizar(id, nome, professores, vagas, nivel):
+    Ncurso.atualizar(curso(id, nome, professores, vagas, nivel))
+
+  def curso_excluir(id):
+    Ncurso.excluir(curso(id, "", "", "", ""))
